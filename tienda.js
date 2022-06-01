3788e5140e886b80136dd7028ddd6704c7edeb9d
Alejo Sosa <alejososa1987@gmail.com>
2022-06-01 19:47:29 -0300

    agregado el fetch

--- tienda.js
+++ tienda.js
@@ -128,3 +128,19 @@ function comprarButtonClicked() {
       )
     }
   })}
+
+
+  // comienza el uso del fetch
+  
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+      'X-RapidAPI-Key': 'd49237a7d4msh8a48402a14f9a1ep165448jsn5b843072ef1e'
+    }
+  };
+
+  fetch('https://weatherapi-com.p.rapidapi.com/future.json?q=London&dt=2022-12-25', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
